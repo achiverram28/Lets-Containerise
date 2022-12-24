a70577187062d8c64ae591de74eef254fe74488b
achiverram28 <97288756+achiverram28@users.noreply.github.com>
2022-12-24 16:08:22 +0530

    Uploaded Windows version of terraform

---
+++ source/Tool/Terraform_Win/terraform_deploy.py
@@ -0,0 +1,7 @@
+import os
+print("You will be spinning up a nginx server...")
+print("Checking the current version of your terraform")
+os.system("terraform version")
+os.system("terraform init")
+os.system("terraform plan")
+os.system("terraform apply")
